eb575adb728b80b2495e749dd2586e9adf10816e
Jesus Laucho <jlaucho@gmail.com>
2018-03-28 06:01:55 -0400

    Aplicando filter

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 // tslint:disable-next-line:import-blacklist
 import { Observable } from 'rxjs/Rx';
-import { setInterval } from 'timers';
-import 'rxjs/add/operator/retry';
+// import { setInterval } from 'timers';
+// import 'rxjs/add/operator/retry';
 
 @Component({
   selector: 'app-rxjs',
@@ -12,25 +12,7 @@ import 'rxjs/add/operator/retry';
 export class RxjsComponent implements OnInit {
 
   constructor() {
-    let obs = new Observable( observar => {
-      let contador = 0;
-      let intervalo = setInterval( () => {
-        contador += 1;
-        observar.next( contador );
-        if ( contador === 3 ) {
-          clearInterval ( intervalo );
-          observar.complete();
-        }
-        if ( contador === 2 ) {
-          clearInterval ( intervalo );
-          observar.error ( 'Auxilio' );
-          // contador = 0;
-        }
-      }, 1000);
-    });
-
-    obs
-    .retry()
+    this.regresaObservable()
     .subscribe(
       numero => {
       console.log( 'Numero', numero );
@@ -45,6 +27,41 @@ export class RxjsComponent implements OnInit {
    }
 
   ngOnInit() {
+  }
+
+  regresaObservable(): Observable<any> {
+    return new Observable( observar => {
+      let contador = 0;
+      let intervalo = setInterval( () => {
+        contador += 1;
+        let salida = {
+          valor: contador
+        };
+        observar.next( salida );
+        if ( contador === 3 ) {
+          clearInterval ( intervalo );
+          observar.complete();
+        }
+        // if ( contador === 2 ) {
+        //   observar.error ( 'Auxilio' );
+        //   // contador = 0;
+        // }
+      }, 1000);
+    })
+    .retry(2)
+    .map( (resp: any) => {
+      return resp.valor;
+    })
+    .filter( (valor, index) => {
+      // console.log( 'Fliter', valor, index );
+      if ( (valor % 2) === 0 ) {
+        // numeros pares
+        return false;
+      } else {
+        // numeros impares
+        return true;
+      }
+    });
   }
 
 }
