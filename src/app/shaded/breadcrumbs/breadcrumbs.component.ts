54b43925b55323fd4bab9199cff3f460a7eed37f
Jesus Laucho <jlaucho@gmail.com>
2018-03-28 08:05:30 -0400

    Lista hasta la seccion 8

--- src/app/shaded/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shaded/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivationEnd } from '@angular/router';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumbsComponent implements OnInit {
 
-  constructor() { }
+  titulo: string;
+
+  constructor( private router: Router, private title: Title, public meta: Meta ) {
+    this.getDataRoute()
+      .subscribe( event => {
+        this.titulo = event.titulo;
+        this.title.setTitle( event.titulo );
+
+        // actualizando los metadatos
+
+        let metadatos: MetaDefinition[] = [{
+          name: 'description',
+          content: event.titulo
+        },
+        {
+          name: 'keyboard',
+          content: 'palabras claves'
+        }
+      ];
+
+        this.meta.addTags(metadatos);
+      });
+   }
 
   ngOnInit() {
   }
 
+  getDataRoute() {
+    return this.router.events
+      .filter( evento => evento instanceof ActivationEnd )
+      .filter( (evento: ActivationEnd ) => evento.snapshot.firstChild === null)
+      .map( (evento: ActivationEnd) => evento.snapshot.data );
+  }
 }
